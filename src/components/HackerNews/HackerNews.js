e17307b777f330e2c8dfebb5f401633a713e9831
Daniel St. Martin <dan-iel@Daniels-MacBook-Air.local>
2019-04-17 13:49:52 -0700

    finished mini

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -1,30 +1,47 @@
-import React, { Component } from 'react';
-import Card from './../shared/Card/Card';
-import Loading from './../shared/Loading/Loading';
+import React, { Component } from "react";
+import Card from "./../shared/Card/Card";
+import Loading from "./../shared/Loading/Loading";
+import { connect } from "react-redux";
+import { requestArticles } from "../../ducks/hackerNewsReducer";
 
 class HackerNews extends Component {
   constructor(props) {
     super(props);
-    this.state = { articles: [], loading: true }
+    this.state = {};
   }
-
+  componentDidMount() {
+    // requestArticlesfunction from reducer that was mapped to props
+    this.props.requestArticles();
+  }
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map(article => (
+      <Card key={article.id} article={article} />
+    ));
     return (
-      <div className='news-container'>
+      <div className="news-container">
         <img style={styles.logo} src="./hackerNews.jpeg" alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
-    )
+    );
   }
 }
-
-export default HackerNews;
-
+// mapStateToProps the state in the reducer to the props in this componenet
+function mapStateToProps(state) {
+  return state.hackerNews;
+}
+// mapDispatchToProps will map functions imported from the reducer onto the props of this components
+const mapDispatchToProps = {
+  requestArticles: requestArticles
+};
+// we export the evoked connect component wit the mapStateToProps, mapDispatchToProps
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HackerNews);
 
 const styles = {
   logo: {
-    width: '250px',
-    margin: '50px 0px'
+    width: "250px",
+    margin: "50px 0px"
   }
-}
+};
--- src/components/Reddit/Reddit.js
+++ src/components/Reddit/Reddit.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
-import Card from './../shared/Card/Card';
-import Loading from './../shared/Loading/Loading';
+import React, { Component } from "react";
+import Card from "./../shared/Card/Card";
+import Loading from "./../shared/Loading/Loading";
+import { connect } from "react-redux";
+import { requestArticles } from "./../../ducks/redditReducer";
 
 class Reddit extends Component {
   constructor(props) {
     super(props);
-    this.state = { articles: [], loading: true }
+    this.state = {};
+  }
+  componentDidMount(){
+    this.props.requestArticles()
   }
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map(article => (
+      <Card key={article.id} article={article} />
+    ));
     return (
-      <div className='news-container'>
-        <img src="./redditLogo.png" alt="" style={styles.logo} />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+      <div className="news-container">
+        <img src="./redditLogo.png" style={styles.logo} alt="" />
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
-    )
+    );
   }
 }
+function mapStateToProps(state) {
+  return state.reddit;
+}
+const mapDispatchToProps = {
+  requestArticles: requestArticles
+};
 
-export default Reddit;
-
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Reddit);
 
 const styles = {
   logo: {
